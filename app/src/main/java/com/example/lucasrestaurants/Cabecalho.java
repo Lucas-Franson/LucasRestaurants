5b9da9fb1b092078a829bd536a6b32ef822abe8c
Lucas-Franson <lucasfranson.8@hotmail.com>
2020-09-29 22:24:06 -0300

    OK

---
+++ app/src/main/java/com/example/lucasrestaurants/Cabecalho.java
@@ -0,0 +1,14 @@
+package com.example.lucasrestaurants;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cabecalho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cabecalho);
+    }
+}
